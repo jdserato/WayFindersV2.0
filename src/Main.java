695b167073416d9353aeae4418544c9c2a055e1f
Jay Vince Serato <jdserato@up.edu.ph>
2017-12-02 00:21:11 +0800

    CODE FREEZE. We are done with this.

--- src/Main.java
+++ src/Main.java
@@ -15,19 +15,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Rectangle2D screen = Screen.getPrimary().getVisualBounds();
-        /*Parent root = FXMLLoader.load(new File("src/Management.fxml").toURL());
+        Parent root = FXMLLoader.load(new File("src/Management.fxml").toURL());
         Scene scene = new Scene(root, 750, 450);
         primaryStage.setScene(scene);
         primaryStage.setTitle("CSBT Data Editor");
-        primaryStage.show();*/
-        Stage second = new Stage();
+        primaryStage.show();
+        /*Stage second = new Stage();
         Parent secondRoot = FXMLLoader.load(new File("src/WayFinders.fxml").toURL());
         Scene sceneTwo = new Scene(secondRoot, screen.getWidth(), screen.getHeight());
         second.setScene(sceneTwo);
         second.setTitle("SBT Directory System");
-        second.setFullScreen(true);
+        second.setFullScreen(true);*/
         //second.setAlwaysOnTop(true);
-        second.show();
+        //second.show();
 
     }
 
